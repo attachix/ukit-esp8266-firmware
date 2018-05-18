40b2fd68236b0cf35f0880658d7d65b2258069df
Slavey Karadzhov <slav@attachix.com>
2018-05-18 17:01:16 +0200

    Initial version of the ESP8266 firmware.

---
+++ tools/copyright.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+copyright-header --license GPL3 \
+--add-path app/:lib/core:lib/jsvm:tools/:include/ \
+--copyright-holder 'Slavey Karadzhov <slav@attachix.com>' \
+--copyright-software 'U:Kit ESP8266 Firmware' \
+--copyright-software-description "This is the 'smart' firmware for the U:Kit sensor kit" \
+--copyright-year 2016 \
+--copyright-year 2018 \
+--word-wrap 80 --output-dir ./
